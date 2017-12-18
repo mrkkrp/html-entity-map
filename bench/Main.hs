d8ee3c511bcaacedee4942875d3ce4b64f61e977
mrkkrp <markkarpov92@gmail.com>
2017-12-18 15:20:44 +0700

    Add a benchmark

---
+++ bench/Main.hs
@@ -0,0 +1,23 @@
+module Main (main) where
+
+import Criterion.Main
+import Data.HTML.Entities
+import qualified Data.Map.Strict as M
+import qualified Data.Text       as T
+
+-- | I used this to decide whether using @unordered-containers@ makes sense
+-- or @containers@ will do.
+
+main :: IO ()
+main = defaultMain
+  [ bnch "AMP"
+  , bnch "Bernoullis"
+  , bnch "HumpDownHump"
+  , bnch "Star"
+  , bnch "downharpoonright" ]
+
+-- | Benchmark looking up an entity with given name.
+
+bnch :: String -> Benchmark
+bnch entity = env (return (T.pack entity)) $
+  bench entity . nf (`M.lookup` htmlEntityMap)
