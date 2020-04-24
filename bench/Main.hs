184ab918c99598675d24d428bb789f2d28e4e470
Mark Karpov <markkarpov92@gmail.com>
2020-04-24 13:31:49 +0200

    Format the source code with Ormolu

--- bench/Main.hs
+++ bench/Main.hs
@@ -3,21 +3,22 @@ module Main (main) where
 import Criterion.Main
 import Data.HTML.Entities
 import qualified Data.HashMap.Strict as M
-import qualified Data.Text           as T
+import qualified Data.Text as T
 
 -- | I used this to decide whether using @unordered-containers@ makes sense
 -- or @containers@ will do.
-
 main :: IO ()
-main = defaultMain
-  [ bnch "AMP"
-  , bnch "Bernoullis"
-  , bnch "HumpDownHump"
-  , bnch "Star"
-  , bnch "downharpoonright" ]
+main =
+  defaultMain
+    [ bnch "AMP",
+      bnch "Bernoullis",
+      bnch "HumpDownHump",
+      bnch "Star",
+      bnch "downharpoonright"
+    ]
 
 -- | Benchmark looking up an entity with given name.
-
 bnch :: String -> Benchmark
-bnch entity = env (return (T.pack entity)) $
-  bench entity . nf (`M.lookup` htmlEntityMap)
+bnch entity =
+  env (return (T.pack entity)) $
+    bench entity . nf (`M.lookup` htmlEntityMap)
